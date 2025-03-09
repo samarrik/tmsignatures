070cd0c02d786549925a13e4bed0095cee5e5575
samarrik <samariva@fit.cvut.cz>
2025-03-09 11:11:19 +0100

    Add logging and environment variables managment

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from tms.utils.env import setup_environment
+from tms.utils.logger import get_logger, setup_logging
+
+setup_environment()
+setup_logging()
+
+logger = get_logger(__name__)
+
+
 def main():
-    pass
+    logger.info("Starting TMSignatures")
+
 
 if __name__ == "__main__":
     main()
